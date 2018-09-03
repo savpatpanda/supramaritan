6de7ce786e6c5582a63e9f6f785098df584e301a
savpatpanda <savandrew1@gmail.com>
2018-09-03 15:41:14 -0400

    commit

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MapView 
+          style={styles.gmap}
+          region={{
+              latitude: 40.442431,
+              longitude: -74.662218,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01
+          }}
+        >
+
+          <MapView.Marker
+            coordinate={{
+              latitude: 40.442431,
+              longitude: -74.662218
+            }}
+            title={'Current location'}
+            description={'My current location'}
+          />
+
+          <MapView.Marker
+            coordinate={{
+              latitude: 40.438036,
+              longitude: -74.655580
+            }}
+            title={'Ivans location'}
+            description={'Ivans house location'}
+          />
+
+        </MapView>
       </View>
     );
   }
@@ -15,9 +41,20 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
+
+  gmap:{
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0
+  }
 });
