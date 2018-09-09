99a00cc47e2a435703e55a6d44887fb07495ae70
savpatpanda <savandrew1@gmail.com>
2018-09-09 00:21:09 -0400

    color of markers

--- components/Main.js
+++ components/Main.js
@@ -83,9 +83,18 @@ class Main extends React.Component {
 
   }
 
-
   setAuthModalVisible(visible) {
     this.setState({authModalVisible: visible});
+  }
+
+  colorMarker(severity){
+      if(severity ==1){
+        return '#efb802'
+      }else if(severity ==2){
+        return '#ef8802'
+      }else{
+        return '#ef4102'
+      }
   }
 
   segueToDetailView(index){
@@ -180,6 +189,7 @@ class Main extends React.Component {
                 longitude: marker.coordinates.long}}
               title={"incident"}
               key={marker.key}
+              pinColor={this.colorMarker(marker.currentPriority)}
               onPress={this.segueToDetailView.bind(this, marker.key)}
             />
           ))}
